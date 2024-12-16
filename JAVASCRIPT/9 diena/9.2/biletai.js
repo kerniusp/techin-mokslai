05232be7f8272e6eb58017a2126382eb86b83b5f
kernius <kpauliukenas@gmail.com>
2024-12-16 15:04:49 +0200

    Add javascript 9.2 sixth task

---
+++ JAVASCRIPT/9 diena/9.2/biletai.js
@@ -0,0 +1,6 @@
+const biletoKaina = +prompt("Iveskite vieno bileto kaina:");
+const uzdirbtiPinigai = +prompt("Iveskite visa sukaupta pinigu suma");
+
+const result = uzdirbtiPinigai / biletoKaina;
+
+console.log(`Isviso zmoniu galit patekti ${result}`);
