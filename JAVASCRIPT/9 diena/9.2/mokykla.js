1081faa99123c5b06505996a5e7264fc76ee6657
kernius <kpauliukenas@gmail.com>
2024-12-16 15:00:04 +0200

    Add javascript 9.2 fifth task

---
+++ JAVASCRIPT/9 diena/9.2/mokykla.js
@@ -0,0 +1,7 @@
+const ilgis = +prompt("Iveskite mokyklos sales ilgi");
+const aukstis = +prompt("Iveskite mokyklos sales auksti");
+
+const plotis = ilgis * aukstis;
+
+console.log(`Mokyklos sporto sales plotis yra ${plotis}`);
+
