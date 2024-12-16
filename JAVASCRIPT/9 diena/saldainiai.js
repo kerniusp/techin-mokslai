193071eac70c3bf85455b7ddf3bee08fce51932c
kernius <kpauliukenas@gmail.com>
2024-12-16 14:14:16 +0200

    Update javascript 10 task variable name

--- JAVASCRIPT/9 diena/saldainiai.js
+++ JAVASCRIPT/9 diena/saldainiai.js
@@ -2,12 +2,12 @@ const mariausSaldainiai = +prompt("Kiek saldainiu gauna Marius");
 const kiekSaldainiuSuvalgo = +prompt("Kiek saldainiu suvalgo Marius");
 const dienosIkiKaledu = +prompt("Kiek dienu liko iki kaledu?");
 
-const SukauptiSaldainiai =
+const sukauptiSaldainiai =
   (mariausSaldainiai - kiekSaldainiuSuvalgo) * dienosIkiKaledu;
 
-const likeSaldainiai = SukauptiSaldainiai % kiekSaldainiuSuvalgo;
+const likeSaldainiai = sukauptiSaldainiai % kiekSaldainiuSuvalgo;
 const dovanotiSaldainiai =
-  (SukauptiSaldainiai - likeSaldainiai) / kiekSaldainiuSuvalgo;
+  (sukauptiSaldainiai - likeSaldainiai) / kiekSaldainiuSuvalgo;
 
 console.log(`Marius dovanas paruos ${dovanotiSaldainiai} draugu`);
 console.log(`Supakavus dovanas liks ${likeSaldainiai} saldainiai`);
