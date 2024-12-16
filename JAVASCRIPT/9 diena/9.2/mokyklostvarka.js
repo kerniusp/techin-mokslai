1ea765ea5274e12a7f16279b9d9f2093d40db355
kernius <kpauliukenas@gmail.com>
2024-12-16 15:25:20 +0200

    Add javascript 9.2 ninth task

---
+++ JAVASCRIPT/9 diena/9.2/mokyklostvarka.js
@@ -0,0 +1,7 @@
+const mokiniai = +prompt("Iveskite skaiciu mokiniu");
+const klasesMokiniai = 20;
+if(mokiniai > klasesMokiniai){
+    console.log("Taip")
+}else if(mokiniai < klasesMokiniai){
+    console.log("Ne");
+}
