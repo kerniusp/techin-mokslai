d177a79c2b0717bc336af5529c9233be0669fbd5
kernius <kpauliukenas@gmail.com>
2024-12-17 12:49:22 +0200

    Add for loop at Javascript task 10.11

--- JAVASCRIPT/10 diena/10.1/dalyba.js
+++ JAVASCRIPT/10 diena/10.1/dalyba.js
@@ -3,6 +3,13 @@ while (i <= 5) {
   if (i != 0) {
     const ats = 1 / i;
     console.log(ats);
-  } 
+  }
   i++;
+}
+
+for (let j = -5; j <= 5; j++) {
+  if (j != 0) {
+    const ats2 = 1 / j;
+    console.log(ats2);
+  }
 }
