59cda13a1ac8de3995f3c7aea44ba072c6f331ba
kernius <kpauliukenas@gmail.com>
2024-12-18 16:07:53 +0200

    Adjust day 11 javascript 8 task function

--- JAVASCRIPT/11 diena/11.8/script.js
+++ JAVASCRIPT/11 diena/11.8/script.js
@@ -2,7 +2,7 @@ const mygtukas = document.querySelector("button");
 const fonas = document.querySelector("body");
 
 const setBg = () => {
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
 };
