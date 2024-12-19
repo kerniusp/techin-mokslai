3ed5a8355d3a75aee7c1a7c54cb3111439f9228f
kernius <kpauliukenas@gmail.com>
2024-12-19 13:23:10 +0200

    Add 12 day javascript 12.6 task

---
+++ JAVASCRIPT/12 diena/12.6/script.js
@@ -0,0 +1,30 @@
+const forma = document.querySelector("form");
+const background = document.querySelector("body");
+
+function nuotrauka(number) {
+  switch (parseInt(number)) {
+    case 1:
+      background.style.backgroundImage = "url(pavasaris.jpg)";
+      break;
+    case 2:
+      background.style.backgroundImage = "url(summer.jpg)";
+      break;
+    case 3:
+      background.style.backgroundImage = "url(ruduo.jpg)";
+      break;
+    case 4:
+      background.style.backgroundImage = "url(ziema.jpg)";
+    default:
+      console.log("testas");
+  }
+}
+console.log(forma);
+console.log(background);
+
+forma.addEventListener("change", function () {
+  const pasirinkimas = document.querySelector("#id-of-select");
+  const pasirinkimoValue = pasirinkimas.value;
+  console.log(pasirinkimoValue);
+
+  nuotrauka(pasirinkimoValue);
+});
