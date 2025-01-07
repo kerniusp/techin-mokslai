cd21be805a32451e5a207a77ce4eec94af71cec6
Kernius <kpauliukenas@gmail.com>
2025-01-07 12:48:30 +0200

    Add java uzduotis 02_08

--- JAVA/2/2.1/uzduotis/02_08.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ JAVA/2/2.1/uzduotis/02_08.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -7,6 +7,16 @@ public class AverageOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+
+        double avg = ((double)firstNumber + secondNumber + thirdNumber) / 3;
+
+        System.out.println("The average is " + avg);
 
     }
 }
