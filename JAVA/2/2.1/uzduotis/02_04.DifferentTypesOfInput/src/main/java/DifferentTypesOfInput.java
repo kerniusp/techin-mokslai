7dc4e49ce7967ef4aa6b8d2243e780390a8ff4e0
Kernius <kpauliukenas@gmail.com>
2025-01-07 12:36:55 +0200

    Add java uzduotis 02_04

--- JAVA/2/2.1/uzduotis/02_04.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ JAVA/2/2.1/uzduotis/02_04.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -7,6 +7,21 @@ public class DifferentTypesOfInput {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a string:");
+        String stringas = scan.nextLine();
 
+        System.out.println("Give an integer:");
+        int skaiciusA = Integer.parseInt(scan.nextLine());
+
+        System.out.println("Give a double:");
+        double skaicius = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Give a boolean:");
+        boolean bool = Boolean.parseBoolean(scan.nextLine());
+
+        System.out.println("You gave the string "+stringas);
+        System.out.println("You gave the integer "+skaiciusA);
+        System.out.println("You gave the double "+skaicius);
+        System.out.println("You gave the boolean "+bool);
     }
 }
