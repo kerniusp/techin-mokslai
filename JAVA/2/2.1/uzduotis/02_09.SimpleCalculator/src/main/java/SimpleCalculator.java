eb0236cc8a4f464d6f212a098c625e97df8b63ff
Kernius <kpauliukenas@gmail.com>
2025-01-07 12:51:52 +0200

    Add java uzduotis 02_09

--- JAVA/2/2.1/uzduotis/02_09.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ JAVA/2/2.1/uzduotis/02_09.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,19 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
 
+        int sum = firstNumber + secondNumber;
+        int subt = firstNumber - secondNumber;
+        int daug = firstNumber * secondNumber;
+        double div = (double)firstNumber / secondNumber;
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + subt);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + daug);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + div);
     }
 }
