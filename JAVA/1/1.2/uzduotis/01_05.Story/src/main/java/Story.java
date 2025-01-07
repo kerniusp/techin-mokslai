1f9812707391480682df82c6cda33545583396f3
Kernius <kpauliukenas@gmail.com>
2025-01-07 12:09:29 +0200

    Add java uzduoti 01_05

--- JAVA/1/1.2/uzduotis/01_05.Story/src/main/java/Story.java
+++ JAVA/1/1.2/uzduotis/01_05.Story/src/main/java/Story.java
@@ -7,6 +7,19 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
 
+        String inputName = scanner.nextLine();
+
+
+        System.out.println("What is their job?");
+        String inputJob = scanner.nextLine();
+
+
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " +inputName+ ", who was " +inputJob +".");
+        System.out.println("On the way to work, " + inputName + " reflected on life.");
+        System.out.println("Perhaps " + inputName + " will not be " + inputJob + " forever.");
     }
 }
