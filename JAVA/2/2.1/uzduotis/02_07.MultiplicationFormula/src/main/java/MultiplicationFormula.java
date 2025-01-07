37a3fd7f1adff8bb3f9d9f9088c2945a60b9753c
Kernius <kpauliukenas@gmail.com>
2025-01-07 12:46:05 +0200

    Add java uzduotis 02_07

--- JAVA/2/2.1/uzduotis/02_07.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ JAVA/2/2.1/uzduotis/02_07.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -7,6 +7,13 @@ public class MultiplicationFormula {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
 
+        int sum = firstNumber * secondNumber;
+
+        System.out.println(firstNumber + " * "+secondNumber + " = " + sum);
     }
 }
