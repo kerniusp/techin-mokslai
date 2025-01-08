fffb03b97de4f95ca44fded46d39b49a0fe9378e
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:22:49 +0200

    Add java uzduotis 04_06

--- JAVA/3/3.2/04_06.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ JAVA/3/3.2/04_06.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,20 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int suma = 0;
+        int number = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0) {
+                break;
+            }
+            suma += number;
+
+
+        }
+        System.out.println("Sum of the numbers: "+suma);
     }
 }
