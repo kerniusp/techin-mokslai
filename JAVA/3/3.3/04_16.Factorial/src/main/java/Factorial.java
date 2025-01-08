ebbca1c7be7fc0f41f71439d54288b2184344a18
Kernius <kpauliukenas@gmail.com>
2025-01-08 13:31:43 +0200

    Add java uzduotis 04_16

--- JAVA/3/3.3/04_16.Factorial/src/main/java/Factorial.java
+++ JAVA/3/3.3/04_16.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        int sum = 1;
+
+        System.out.println("Give a number");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 1; i <= number; i++){
+            sum *= i;
+            System.out.println(sum);
+        }
+        System.out.println("Factorial: "+sum);
     }
 }
