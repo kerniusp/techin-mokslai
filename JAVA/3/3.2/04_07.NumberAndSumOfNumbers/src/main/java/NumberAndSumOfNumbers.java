252b24a372b64dfcdab5bf20c2bc29f48fdb229f
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:25:40 +0200

    Add java uzduotis 04_07

--- JAVA/3/3.2/04_07.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ JAVA/3/3.2/04_07.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,22 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        int sum = 0;
+        int number = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0){
+                break;
+            }
+            counter ++;
+            sum += number;
+
+        }
+        System.out.println("Number of numbers: "+counter);
+        System.out.println("Sum of the numbers: "+sum);
     }
 }
