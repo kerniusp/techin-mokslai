e4beed20a59d61c56c0c4920470babb19bb6e2fa
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:08:39 +0200

    Add java uzduotis 04_03

--- JAVA/3/3.2/04_03.OnlyPositives/src/main/java/OnlyPositives.java
+++ JAVA/3/3.2/04_03.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,18 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        while(true){
+            System.out.println("Give a number: ");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if(number < 0){
+                System.out.println("Unsuitable number");
+            }else if(number > 0){
+                System.out.println(Math.pow(number,2));
+            }else{
+                break;
+            }
+        }
     }
 }
