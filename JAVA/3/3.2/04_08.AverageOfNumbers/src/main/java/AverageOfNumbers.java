8dbcb3b79875c5861d39d49f36a883b8187643a8
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:28:10 +0200

    Add java uzduotis 04_08

--- JAVA/3/3.2/04_08.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ JAVA/3/3.2/04_08.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,23 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        int sum = 0;
+        int number = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0){
+                break;
+            }
+            counter ++;
+            sum += number;
+
+        }
+
+        double avg = (double)sum / counter;
+        System.out.println("Average of the numbers: "+avg);
     }
 }
