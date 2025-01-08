da2a08742770bda0a97039677c90363a6f26cf7e
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:04:50 +0200

    Add java uzduotis 04_02

--- JAVA/3/3.2/04_02.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ JAVA/3/3.2/04_02.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -6,5 +6,13 @@ public class AreWeThereYet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.println("Give a number");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 4){
+                break;
+            }
+        }
     }
 }
