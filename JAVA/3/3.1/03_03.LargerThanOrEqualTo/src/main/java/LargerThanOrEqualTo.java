8f62a793f8229fd129d1a822c04a8b19575c58e9
Kernius <kpauliukenas@gmail.com>
2025-01-08 08:36:59 +0200

    Add java uzduotis 03_03

--- JAVA/3/3.1/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ JAVA/3/3.1/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -6,5 +6,18 @@ public class LargerThanOrEqualTo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(scan.nextLine());
+
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(scan.nextLine());
+
+        if(number1 > number2){
+            System.out.println("Greater number is: "+number1);
+        }else if(number2 > number1){
+            System.out.println("Greater number is: "+number2);
+        }else{
+            System.out.println("The numbers are equal!");
+        }
     }
 }
