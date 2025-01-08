cbfd5ac147422f2dce747043de9ec44d2c484a19
Kernius <kpauliukenas@gmail.com>
2025-01-08 13:20:04 +0200

    Add java uzduotis 04_13

--- JAVA/3/3.3/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ JAVA/3/3.3/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,16 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to?");
+        int whereTo = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Where from?");
+        int whereFrom = Integer.parseInt(scanner.nextLine());
+
+        for(int i = whereFrom; i <= whereTo; i++){
+            System.out.println(i);
+        }
+
+
     }
 }
