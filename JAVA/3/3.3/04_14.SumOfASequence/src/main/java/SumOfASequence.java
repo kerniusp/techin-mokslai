9d35ff29818ca311d37ac0764b19e7fdce6b9cf5
Kernius <kpauliukenas@gmail.com>
2025-01-08 13:23:46 +0200

    Add java uzduotis 04_14

--- JAVA/3/3.3/04_14.SumOfASequence/src/main/java/SumOfASequence.java
+++ JAVA/3/3.3/04_14.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,14 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
+        System.out.println("Last number?");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 1; i <= number; i++){
+            sum += i;
+        }
+        System.out.println("The sum is "+sum);
     }
 }
