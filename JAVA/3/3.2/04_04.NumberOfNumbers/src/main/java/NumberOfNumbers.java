83edd144064a083fcc3df58d266a7ca36a5b372a
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:17:25 +0200

    Add java uzduotis 04_04

--- JAVA/3/3.2/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ JAVA/3/3.2/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,19 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0) {
+                break;
+            }else{
+                counter += 1;
+            }
+
+        }
+        System.out.println("Number of numbers: "+counter);
 
     }
 }
