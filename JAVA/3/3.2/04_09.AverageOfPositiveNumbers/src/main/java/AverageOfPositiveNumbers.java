3c01d9427905f67b02453ed08be0c32b3c57bb8a
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:41:08 +0200

    Add java uzduotis 04_09

--- JAVA/3/3.2/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ JAVA/3/3.2/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,25 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int positiveCounter = 0;
+        int number = 0;
+        int sum = 0;
+
+        while(true){
+            number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0){
+                break;
+            }else if(number > 0){
+                positiveCounter++;
+                sum +=number;
+            }
+        }
+        
+        if(positiveCounter > 0){
+            System.out.println((double)sum / positiveCounter);
+        }else{
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
