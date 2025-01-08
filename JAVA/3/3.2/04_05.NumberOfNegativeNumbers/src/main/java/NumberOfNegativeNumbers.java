6665378115943f6326a2bbb4bb85dfd9b503332c
Kernius <kpauliukenas@gmail.com>
2025-01-08 12:19:17 +0200

    Add java uzduotis 04_05

--- JAVA/3/3.2/04_05.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ JAVA/3/3.2/04_05.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,18 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if(number == 0) {
+                break;
+            }else if(number < 0){
+                counter += 1;
+            }
+
+        }
+        System.out.println("Number of negative numbers: "+counter);
     }
 }
