f071e418c413dfb04a29578306a25f161cdc2f8f
Kernius <kpauliukenas@gmail.com>
2025-01-08 08:49:21 +0200

    Add java 03_04 uzduoti

--- JAVA/3/3.1/03_04.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ JAVA/3/3.1/03_04.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -6,5 +6,25 @@ public class GradesAndPoints {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give points [0-100]:");
+        int number = Integer.parseInt(scan.nextLine());
+
+        if(number > 100){
+            System.out.println("incredible!");
+        }else if(number >= 90){
+            System.out.println("5");
+        }else if(number >= 80){
+            System.out.println("4");
+        }else if(number >= 70){
+            System.out.println("3");
+        }else if(number >= 60){
+            System.out.println("2");
+        }else if(number >= 50){
+            System.out.println("1");
+        }else if(number <= 49 && number >= 0){
+            System.out.println("failed");
+        }else{
+            System.out.println("impossible!");
+        }
     }
 }
