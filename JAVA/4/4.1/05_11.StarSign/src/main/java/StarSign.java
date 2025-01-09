a05603e5b918d3b1c02f726de161d48af1ffff75
Kernius <kpauliukenas@gmail.com>
2025-01-09 12:13:32 +0200

    Add java uzduotis 05_13

--- JAVA/4/4.1/05_11.StarSign/src/main/java/StarSign.java
+++ JAVA/4/4.1/05_11.StarSign/src/main/java/StarSign.java
@@ -7,30 +7,53 @@ public class StarSign {
         //The tests are not checking the main, so you can modify it freely.
         //NB: If the tests don't seem to pass, you should try the methods here
         //in the main to make sure they print the correct shapes!
-        
         printStars(3);
-        System.out.println("\n---");  // printing --- between the shapes
+        System.out.println("---");  // printing --- between the shapes
         printSquare(4);
-        System.out.println("\n---");
+        System.out.println("---");
         printRectangle(5, 6);
-        System.out.println("\n---");
+        System.out.println("--");
         printTriangle(3);
-        System.out.println("\n---");
+        System.out.println("---");
     }
 
     public static void printStars(int number) {
         // first part of the exercise
+
+        for(int i = 1; i <= number; i ++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
     public static void printSquare(int size) {
         // second part of the exercise
+        for(int i = 1; i <= size; i ++){
+            for (int j = 1; j <= size; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
     public static void printRectangle(int width, int height) {
         // third part of the exercise
+        for(int i = 1; i <= height; i++){
+            for (int j = 1; j <= width; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
     public static void printTriangle(int size) {
         // fourth part of the exercise
+        for(int i = 1; i <= size; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
     }
 }
