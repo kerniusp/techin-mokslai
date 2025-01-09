aed8b2c2268a520febd26aa53addd01e53762e08
Kernius <kpauliukenas@gmail.com>
2025-01-09 11:11:37 +0200

    Add java uzduotis 05_06

--- JAVA/4/4.1/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ JAVA/4/4.1/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -6,6 +6,22 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter beginning");
+        int beginning = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter end");
+        int end = Integer.parseInt(scanner.nextLine());
+
+        divisibleByThreeInRange(beginning,end);
+    }
+
+    public static void divisibleByThreeInRange(int numberStart, int numberEmd){
+
+        for(int i = numberStart; i <= numberEmd; i ++){
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }
     }
 
 }
