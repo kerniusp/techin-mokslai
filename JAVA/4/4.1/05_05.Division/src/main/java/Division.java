22f402d29b3d55c8753af7dd24e263f8b1a490fd
Kernius <kpauliukenas@gmail.com>
2025-01-09 11:06:01 +0200

    Add java uzduotis 05_05

--- JAVA/4/4.1/05_05.Division/src/main/java/Division.java
+++ JAVA/4/4.1/05_05.Division/src/main/java/Division.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class Division {
 
@@ -7,9 +7,22 @@ public class Division {
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a numerator");
+        int numerator = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter a denominator");
+        int denominator = Integer.parseInt(scanner.nextLine());
         // division(3, 5);
+        division(numerator,denominator);
+
     }
 
     // implement the method here
+    public static void division(int numb1, int numb2){
+
+        System.out.println((double)numb1 / numb2);
+
+    }
 }
