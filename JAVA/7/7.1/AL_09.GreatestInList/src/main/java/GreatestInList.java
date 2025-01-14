ec30cf1eb02453507bf44e535012e65d7d2ce254
Kernius <kpauliukenas@gmail.com>
2025-01-14 12:36:50 +0200

    Add java uzduotis 7_09

--- JAVA/7/7.1/AL_09.GreatestInList/src/main/java/GreatestInList.java
+++ JAVA/7/7.1/AL_09.GreatestInList/src/main/java/GreatestInList.java
@@ -9,14 +9,26 @@ public class GreatestInList {
 
         ArrayList<Integer> list = new ArrayList<>();
         while (true) {
+
             int input = Integer.valueOf(scanner.nextLine());
+
             if (input == -1) {
+
+                int biggestNumber = list.get(0);
+
+                for (int i = 1; i < list.size(); i++) {
+
+                    if (biggestNumber < list.get(i)) {
+                        biggestNumber = list.get(i);
+                    }
+                }
+                System.out.println("The greatest number: " + biggestNumber);
                 break;
             }
 
             list.add(input);
         }
-        
+
         System.out.println("");
 
         // implement finding the greatest number in the list here
