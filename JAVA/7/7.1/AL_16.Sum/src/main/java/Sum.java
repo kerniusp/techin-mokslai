cbf78922989c63aa9b88f5c8776a9a6d4264e1af
Kernius <kpauliukenas@gmail.com>
2025-01-14 13:46:36 +0200

    Add java uzduotis 07_16

--- JAVA/7/7.1/AL_16.Sum/src/main/java/Sum.java
+++ JAVA/7/7.1/AL_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,19 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numb = new ArrayList<>();
+        numb.add(1);
+        numb.add(2);
+        numb.add(3);
+        System.out.println(sum(numb));
     }
 
+    public static int sum(ArrayList<Integer> numbers) {
+
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
+        }
+        return sum;
+    }
 }
