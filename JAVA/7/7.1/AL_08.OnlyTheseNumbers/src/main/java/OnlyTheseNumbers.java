2ec0067c653c1fd80e2f0ce61948c3b5aa73d176
Kernius <kpauliukenas@gmail.com>
2025-01-14 12:22:12 +0200

    Add java uzduotis 7_08

--- JAVA/7/7.1/AL_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ JAVA/7/7.1/AL_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -11,6 +11,19 @@ public class OnlyTheseNumbers {
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
             if (number == -1) {
+                System.out.println("From where? ");
+                int startNumber = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("To where? ");
+                int endNumber = Integer.parseInt(scanner.nextLine());
+
+
+                for (int i = 0; i < numbers.size(); i++) {
+                    if (i >= startNumber && i <= endNumber) {
+                        System.out.println(numbers.get(i));
+
+                    }
+                }
                 break;
             }
 
