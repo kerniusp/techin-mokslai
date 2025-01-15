b28757067cbba760b004b028393353d844b97552
Kernius <kpauliukenas@gmail.com>
2025-01-15 13:42:08 +0200

    Add java uzduotis 8_2_01

---
+++ JAVA/8/8.2/08_2_01.Items/src/main/java/Items.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Items {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the class Item
+
+        ArrayList<Item> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if (name.equals("")) {
+                break;
+            }
+            Item vardas = new Item(name);
+            items.add(vardas);
+            
+        }
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println(items.get(i));
+        }
+
+    }
+}
