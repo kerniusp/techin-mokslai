b5a85f789ceb026ce57bbcc93ead2a5594762897
kernius <kpauliukenas@gmail.com>
2025-01-15 23:18:49 +0200

    Add java uzduotis 08_02_08

--- JAVA/8/8.2/08_2_08.HealthStation/src/main/java/Main.java
+++ JAVA/8/8.2/08_2_08.HealthStation/src/main/java/Main.java
@@ -3,5 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental code here to check how your program functions
+        HealthStation childerHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan",1,110,7);
+        Person peter = new Person("Peter",33,176,85);
+
+        System.out.println(ethan.getName() + " weight: " + childerHospital.weigh(ethan) + " kilos");
+
+        childerHospital.feed(ethan);
+        childerHospital.feed(ethan);
+        childerHospital.feed(ethan);
+
+        System.out.println(ethan.getName() + " weight: "+childerHospital.weigh(ethan)+ " kilos");
     }
 }
