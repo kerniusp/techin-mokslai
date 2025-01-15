18f9c98b7dd429db566503a84c55c55bf723475c
Kernius <kpauliukenas@gmail.com>
2025-01-15 14:15:23 +0200

    Add java uzduotis 08_2_03

--- JAVA/8/8.2/08_2_03.TelevisionPrograms/src/main/java/Main.java
+++ JAVA/8/8.2/08_2_03.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,30 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            System.out.print("Name: ");
+            String programsName = scanner.nextLine();
+
+            if (programsName.equals("")) {
+                break;
+            }
+
+            System.out.print("Duration: ");
+            int programsDuration = Integer.parseInt(scanner.nextLine());
+
+            TelevisionProgram program = new TelevisionProgram(programsName, programsDuration);
+            programs.add(program);
+        }
+
+        System.out.print("Programs maximum duration?");
+        int maxDuration = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < programs.size(); i++) {
+            if (programs.get(i).getDuration() <= maxDuration) {
+                System.out.println(programs.get(i));
+            }
+        }
+
     }
 }
