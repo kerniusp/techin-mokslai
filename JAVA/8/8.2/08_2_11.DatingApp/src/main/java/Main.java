89777b32d1796277576de9e9e1c5b1dde1ca3be0
Kernius <kpauliukenas@gmail.com>
2025-01-16 13:51:16 +0200

    Add java uzduotis 08_2_11

--- JAVA/8/8.2/08_2_11.DatingApp/src/main/java/Main.java
+++ JAVA/8/8.2/08_2_11.DatingApp/src/main/java/Main.java
@@ -3,5 +3,21 @@ public class Main {
 
     public static void main(String[] args) {
         // test your program here!
+        SimpleDate date = new SimpleDate(30, 12, 2011);
+
+        date.advance(3);
+
+        System.out.println(date);
+
+//        System.out.println("Friday of the examined week is " + date);
+////
+//        SimpleDate newDate = date.afterNumberOfDays(7);
+//        int week = 1;
+//        while(week<= 7){
+//            System.out.println("Friday after " + week + " week is " + newDate);
+//            newDate = newDate.afterNumberOfDays(7);
+//            week = week + 1;
+//        }
+//        System.out.println("The date after 790 days from the examined Friday is ... try it out yourself! ");
     }
 }
--- JAVA/8/8.2/08_2_11.DatingApp/src/main/java/SimpleDate.java
+++ JAVA/8/8.2/08_2_11.DatingApp/src/main/java/SimpleDate.java
@@ -33,4 +33,55 @@ public class SimpleDate {
         return false;
     }
 
+    public void advance(){
+        if(day < 30){
+            day++;
+        }else{
+            month++;
+            day -=29;
+        }
+
+        if(month == 12){
+            month = 12;
+        }else if(month > 12){
+            month -= 12;
+            year++;
+        }
+
+    }
+
+    public void advance (int howManyDays){
+
+        if(day < 30){
+            day+= howManyDays;
+        }else{
+            month++;
+            day -= 30 - howManyDays;
+        }
+
+        if(month == 12){
+            month = 12;
+        }else if(month > 12){
+            month -= 12;
+            year++;
+        }
+
+    }
+
+    public SimpleDate afterNumberOfDays(int days){
+
+        SimpleDate newDate = new SimpleDate(this.day, month, year);
+        newDate.advance(days);
+
+        if(newDate.day >= 30) {
+            newDate.day -= 30;
+            newDate.month++;
+            return newDate;
+        }
+            return newDate;
+
+
+
+    }
+
 }
