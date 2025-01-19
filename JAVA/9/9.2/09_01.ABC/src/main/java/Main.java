1a237e1d7883bc6c711996476a2a0ef0990ab2ce
kernius <kpauliukenas@gmail.com>
2025-01-19 16:02:57 +0200

    Add java uzduotis 9_01

--- JAVA/9/9.2/09_01.ABC/src/main/java/Main.java
+++ JAVA/9/9.2/09_01.ABC/src/main/java/Main.java
@@ -2,7 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
-        
+//        // write your test code here
+//        A a = new A();
+//        B b = new B();
+        C c = new C();
+
+        c.a();
+        c.b();
+        c.c();
     }
 }
