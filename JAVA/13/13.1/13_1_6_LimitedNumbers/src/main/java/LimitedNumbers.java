982a1769722816a47ce1e57a614e8b81b4843410
Kernius <kpauliukenas@gmail.com>
2025-01-23 13:12:22 +0200

    Add java uzduotis 13_1_6

--- JAVA/13/13.1/13_1_6_LimitedNumbers/src/main/java/LimitedNumbers.java
+++ JAVA/13/13.1/13_1_6_LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class LimitedNumbers {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        List<Integer> listOfNumbers = new ArrayList<>();
+
+        while (true) {
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input < 0) {
+                break;
+            }
+            listOfNumbers.add(input);
+        }
+        listOfNumbers.stream()
+                .filter(number -> number >= 1 && number <= 5)
+                .forEach(System.out::println);
     }
 }
