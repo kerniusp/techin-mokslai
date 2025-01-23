27f258b1636990908de633a0a595c737ab55f99c
Kernius <kpauliukenas@gmail.com>
2025-01-23 11:50:37 +0200

    Add java uzduotis 13_1_3

--- JAVA/13/13.1/13_1_3_PositiveNumbers/src/main/java/PositiveNumbers.java
+++ JAVA/13/13.1/13_1_3_PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PositiveNumbers {
 
     public static void main(String[] args) {
 
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(-1, -2, 3, 4, -10, 5));
+
+
+        System.out.println(positive(numbers));
+
     }
 
+    public static List<Integer> positive(List<Integer> numbers) {
+
+        List<Integer> newlist = numbers.stream()
+                .filter(number -> number > 0)
+                .collect(Collectors.toList());
+
+        return newlist;
+    }
 }
