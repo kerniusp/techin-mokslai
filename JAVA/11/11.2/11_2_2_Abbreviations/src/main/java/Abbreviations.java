d370b3e1662c8d02ba14e4f210be500a3fe63862
Kernius <kpauliukenas@gmail.com>
2025-01-24 10:50:19 +0200

    Add java uzduotis 11_2_2

---
+++ JAVA/11/11.2/11_2_2_Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Abbreviations {
+
+    Map<String, String> abbrev;
+
+    public Abbreviations() {
+        this.abbrev = new HashMap<>();
+    }
+
+    public void addAbbreviation(String abbrevation, String explanation) {
+        abbrev.put(abbrevation, explanation);
+
+    }
+
+    public boolean hasAbbreviation(String abbrevation) {
+
+        if (abbrev.containsKey(abbrevation)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String findExplanationFor(String abbrevation) {
+
+        if (abbrev.containsKey(abbrevation)) {
+            String value = abbrev.get(abbrevation);
+            return value;
+        }
+        return null;
+    }
+
+
+}
